032a4395be1d82f5874bba7cdb24a067356ff24a
theo <91856536+tkaleel@users.noreply.github.com>
2021-11-28 18:09:35 -0800

    intial commit

---
+++ MyOwnJavaProgram.java
@@ -0,0 +1,8 @@
+public class MyOwnJavaProgram {
+    public static void main(String[] args) {
+
+        System.out.println("my name is theo");
+        System.out.println("I am 33 years old");
+        System.out.println("My hometown is Springfield, IL");
+    }
+}
